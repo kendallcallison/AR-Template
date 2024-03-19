aca3afa3a8604f77d42a268427b964a0e2364865
Kendall Callison <102497336+kendallcallison@users.noreply.github.com>
2024-03-19 09:41:12 -0700

    art gallery

---
+++ arfoundation-samples-5.1/Assets/Scripts/Runtime/ARPlaceObject.cs
@@ -0,0 +1,92 @@
+using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
+using System.Collections;
+
+namespace UnityEngine.XR.ARFoundation.Samples
+
+{
+    /// <summary>
+    /// Subscribes to an <see cref="ARRaycastHitEventAsset"/>. When the event is raised, the
+    /// <see cref="prefabToPlace"/> is instantiated at, or moved to, the hit position.
+    /// </summary>
+    public class ARPlaceObject : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("The prefab to be placed or moved.")]
+        GameObject m_PrefabToPlace;
+
+        [SerializeField]
+        [Tooltip("The pictures to be placed or moved.")]
+        GameObject[] m_Pictures;
+
+        [SerializeField]
+        [Tooltip("The Scriptable Object Asset that contains the ARRaycastHit event.")]
+        ARRaycastHitEventAsset m_RaycastHitEvent;
+
+        GameObject m_SpawnedObject;
+
+        static int imageIndex = 0;
+        bool canPlace = true;
+
+        /// <summary>
+        /// The prefab to be placed or moved.
+        /// </summary>
+        public GameObject prefabToPlace
+        {
+            get => m_PrefabToPlace;
+            set => m_PrefabToPlace = value;
+        }
+
+        /// <summary>
+        /// The spawned prefab instance.
+        /// </summary>
+        public GameObject spawnedObject
+        {
+            get => m_SpawnedObject;
+            set => m_SpawnedObject = value;
+        }
+
+        void OnEnable()
+        {
+            if (m_RaycastHitEvent == null || m_PrefabToPlace == null)
+            {
+                Debug.LogWarning($"{nameof(ARPlaceObject)} component on {name} has null inputs and will have no effect in this scene.", this);
+                return;
+            }
+
+            if (m_RaycastHitEvent != null)
+                m_RaycastHitEvent.eventRaised += PlaceObjectAt;
+        }
+
+        void OnDisable()
+        {
+            if (m_RaycastHitEvent != null)
+                m_RaycastHitEvent.eventRaised -= PlaceObjectAt;
+        }
+
+        void PlaceObjectAt(object sender, ARRaycastHit hitPose)
+        {
+            if (canPlace)
+            {
+                imageIndex++;
+                Quaternion rotation = Quaternion.Euler(0, hitPose.pose.rotation.eulerAngles.y + 270, 0);
+                Vector3 position = hitPose.pose.position;
+                // Use the rotation data as needed
+                // For example, you can extract euler angles:
+                //Vector3 eulerRotation = rotation.eulerAngles;
+                //float xAngle = eulerRotation.x;
+                //float yAngle = eulerRotation.y;
+                //float zAngle = eulerRotation.z;
+                m_SpawnedObject = Instantiate(m_Pictures[imageIndex % m_Pictures.Length], position, rotation, hitPose.trackable.transform.parent);
+            }
+            StartCoroutine(WaitForNextPlacement());
+
+        }
+
+        IEnumerator WaitForNextPlacement()
+        {
+            canPlace = false;
+            yield return new WaitForSeconds(3f);
+            canPlace = true;
+        }
+    }
+}
